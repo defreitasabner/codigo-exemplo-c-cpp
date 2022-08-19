3541b34e132aa2732bab82e2a21c0c6120b39b7b
Abner Freitas <defreitasabner@gmail.com>
2022-08-19 16:08:16 -0300

    Adicionado exemplo de operações matemáticas.

---
+++ operadoresMatematicos/operacoesBasicas.h
@@ -0,0 +1,25 @@
+// Exemplo de função que realiza a adição
+int adicao(int primeiroNumero, int segundoNumero) {
+    return primeiroNumero + segundoNumero;
+}
+
+// Exemplo de função que realiza a subtração
+int subtracao(int primeiroNumero, int segundoNumero) {
+    return primeiroNumero - segundoNumero;
+}
+
+// Exemplo de função que realiza a multiplicação
+int multiplicao(int primeiroNumero, int segundoNumero) {
+    return primeiroNumero * segundoNumero;
+}
+
+// Exemplo de função que realiza a divisão
+// Divisão padrão sempre retorna um float
+float divisao(int dividendo, int divisor) {
+    return dividendo / divisor;
+}
+
+// Exemplo de função que retorna o resto da divisão
+int restoDaDivisao(int dividendo, int divisor) {
+    return dividendo % divisor;
+}
